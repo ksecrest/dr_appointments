55aee3ced6da8254153111ebb336733567e9ef7a
John Doe <paytonanthony2020@gmail.com>
2021-06-10 19:09:57 -0600

    Built all the controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     # user is the co-parent with doctors of appointments
-    has_many :appointments
+    has_many :appointments, dependent: :destroy
     has_many :doctors, through: :appointments
 
     validates :full_name, :dob, :address, :email, presence: true
